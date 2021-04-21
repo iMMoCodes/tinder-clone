5f216e6ad51dd0ac917a9d5a67c7d648c7e2bf89
iMMoCodes <74028194+iMMoCodes@users.noreply.github.com>
2021-04-21 17:22:00 +0300

    connected frontend to backend

--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axiosInstance from '../../axios/axios'
 
 import TinderCard from 'react-tinder-card'
 
 import './Cards.css'
 
 const Cards = () => {
-	const [people, setPeople] = useState([
-		{
-			name: 'Elon Musk',
-			url: 'https://images.cdn.yle.fi/image/upload//w_1199,h_781,f_auto,fl_lossy,q_auto:eco/13-3-10292974.jpg',
-		},
-		{
-			name: 'Jeff Bezos',
-			url:
-				'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTY2NzA3ODE3OTgwMzcyMjYw/jeff-bezos-andrew-harrer_bloomberg-via-getty-images.jpg',
-		},
-	])
+	const [people, setPeople] = useState([])
+
+	const fetchData = async () => {
+		const req = await axiosInstance.get('/tinder/cards')
+
+		setPeople(req.data)
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [])
 
 	const swiped = (direction, nameToDelete) => {
 		console.log('removing' + nameToDelete)
@@ -37,8 +38,8 @@ const Cards = () => {
 						onSwipe={(dir) => swiped(dir, person.name)}
 						onCardLeftScreen={() => outOfFrame(person.name)}
 					>
-						<div style={{ backgroundImage: `url(${person.url})` }} className='card'>
-							<h3>{person.name}</h3>
+						<div style={{ backgroundImage: `url(${person.imgUrl})` }} className='card'>
+							<h1>{person.name}</h1>
 						</div>
 					</TinderCard>
 				))}
